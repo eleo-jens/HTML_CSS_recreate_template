40179cb40b0f135da05718c3ff7f2059b6950340
Eleonore <eleonore.jens@gmail.com>
2022-05-31 19:51:57 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,59 @@
+<!-- Exercice: copy of this template: https://nicepage.com/css-templates/preview/learn-cooking-online-90820?device=desktop -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Template Copy</title>
+    <link rel="stylesheet" href="./assets/css/reset.css">
+    <link rel="stylesheet" href="./assets/css/style.css">
+</head>
+<body>
+
+<nav role="navigation" class="menu">
+    <ul>
+      <li><a href="#">Accueil</a></li>
+      <li><a href="#">Categories</a>
+        <ul>
+          <li><a href="#">Base Meals</a></li>
+          <li><a href="#">Knife Skills</a></li>
+          <li><a href="#">Cooking Veggies</a></li>
+        </ul>
+      </li>
+      <li><a href="#">Learn More</a></li>
+      <li><a href="#">Recherche</a></li>
+      <li><a href="#">À propos</a></li>
+      <li class="droite"><a href="#">Nous Contacter</a></li>
+    </ul​>
+  </nav>
+
+<div class="container">
+    <h1>Learn <b>Cooking</b> Online</h1>
+    <p class="sous-titre">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p>
+    <p class="link">Image from <a href="https://www.freepik.com/free-photos-vectors/food" class="link">Freepik</a></p>
+    <div class="box un">
+        <h2>1</h2>
+        <h3>Base Meals</3>
+        <p class="box">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+        <a href=# class="box">Learn more</a>
+    </div>
+    <div class="box deux">
+        <h2 class="deux">2</h2>
+        <h3>Knife Skills</3>
+        <p class="box">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+        <a href=# class="box">Learn more</a>
+    </div>
+    <div class="box trois">
+        <h2>3</h2>
+        <h3>Cooking Veggies</3>
+        <p class="box">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+        <a href=# class="box trois">Learn more</a>
+    </div>
+</div>
+
+<!-- rajouter un footer avec la source du template original -->
+
+</body>
+</html>
